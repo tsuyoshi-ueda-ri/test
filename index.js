4df64a7f5ec1d8b18be41c7078699aa1e3ede840
上田　剛史 <uedatsuyoshi@uedatakeshinoMacBook-Air.local>
2021-02-25 00:03:33 +0900

    Let's make a while statement

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ let test = 'Hello World!';
 
 //定数
 const testest = 'He..Hell...Hello World!! '
-console.log(testest);
+
+//配列
+const inoki = ['いーち','にーい','さーん','ダーー！！'];
+
+//ループ文
+let index = 0;
+while(index < inoki.length){
+  //繰り返したい命令
+  console.log(inoki[index]);
+  index++;
+}
